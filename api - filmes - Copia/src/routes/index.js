d8ad739d9eb7bc34d49e77413a3c0158154b9964
André Parada <andregparada@gmail.com>
2023-07-05 17:26:25 -0300

    Add files via upload

---
+++ api - filmes - Copia/src/routes/index.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+
+const usersRouter = require("./user.routes");
+const notesRouter = require("./movie_notes.routes");
+const tagsRouter = require("./movie_tags.routes");
+
+const routes = Router();
+routes.use("/users", usersRouter);
+routes.use("/movie_notes", notesRouter);
+routes.use("/movie_tags", tagsRouter)
+
+module.exports = routes;
